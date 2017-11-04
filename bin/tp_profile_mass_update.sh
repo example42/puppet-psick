3a6fd51887b36ebf2ae37820ac9fbfe33335de43
Alessandro Franceschi <al@example42.com>
2017-11-04 17:46:52 +0100

    Refactored tp profiles generation script to allow usage of different pdk templates

--- bin/tp_profile_mass_update.sh
+++ bin/tp_profile_mass_update.sh
@@ -2,8 +2,11 @@
 repo_dir="$(dirname $0)/.."
 . "${repo_dir}/bin/functions"
 cd $repo_dir
-for a in $(cat bin/tp_profile_mass_update.txt); do
+while read line
+do
+  a=$(echo $line | cut -f 1 -d ' ')
+  b=$(echo $line | cut -f 2 -d ' ')
   rm -f manifests/$a/tp.pp
   rm -f spec/classes/$a/tp_spec.rb
-  bin/tp_profile.generate.sh $a
-done
+  bin/tp_profile.generate.sh $a $b
+done < bin/tp_profile_mass_update.txt
