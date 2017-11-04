144faf19cae693aedae4eadc62f169f844e785d1
Alessandro Franceschi <al@example42.com>
2017-11-04 17:55:05 +0100

    Added ansible and sysdig tp profiles

--- spec/classes/sysdig/tp_spec.rb
+++ spec/classes/sysdig/tp_spec.rb
@@ -5,11 +5,9 @@ facts = YAML.load_file(facts_yaml)
 
 default_params = {
   'ensure'             => 'present',
+  'options_hash'       => {},
   'settings_hash'      => {},
   'auto_prereq'        => true,
-}
-sample_settings_hash = {
-  'package_name'     => 'my_sysdig',
 }
 
 describe 'psick::sysdig::tp' do
@@ -35,22 +33,6 @@ describe 'psick::sysdig::tp' do
         it { is_expected.to contain_tp__install('sysdig').with(default_params.merge('ensure' => 'absent')) }
       end
 
-      describe 'with custom settings_hash' do
-        let(:params) do
-          {
-            'settings_hash' => sample_settings_hash,
-          }
-        end
-
-        it { is_expected.to contain_tp__install('sysdig').with(default_params.merge('settings_hash' => sample_settings_hash)) }
-        it { is_expected.to contain_package('my_sysdig').with('ensure' => 'present') }
-      end
-
-      describe 'with auto_prereq => false' do
-        let(:params) { { 'auto_prereq' => false } }
-
-        it { is_expected.to contain_tp__install('sysdig').with(default_params.merge('auto_repo' => false, 'auto_prereq' => false)) }
-      end
     end
   end
 end
