cc85f6d28640ad0713d279f2531996b26a39da71
Alessandro Franceschi <al@example42.com>
2017-11-11 09:54:43 +0100

    Improvements to nfs profiles

--- manifests/nfs/client.pp
+++ manifests/nfs/client.pp
@@ -1,5 +1,20 @@
+# @summary Manage NFS client and mounts
 #
-class psick::nfs::client () {
+# @param mounts_hash An hash of mountpoints to pass
+#  to psick::nfs::mount define
+#
+# @example Client configuration with mount
+#    psick::profiles::linux_classes:
+#      nfs_client: psick::nfs::client
+#
+#    psick::nfs::client::mounts_hash:
+#      data:
+#        server: 10.42.42.101
+#        share: /data
+#        mountpoint: /mnt/data
+class psick::nfs::client (
+  Hash $mounts_hash = {},
+) {
 
   # Workaround for rcpbind service handling.
   tp::install { 'nfs-client':
@@ -8,4 +23,9 @@ class psick::nfs::client () {
     }
   }
 
+  $mounts_hash.each |$k,$v| {
+    psick::nfs::mount { $k:
+      * => $v,
+    }
+  }
 }
--- manifests/nfs/server.pp
+++ manifests/nfs/server.pp
@@ -1,6 +1,25 @@
+# @summary Manages nfs server
 #
-class psick::nfs::server () {
+# @param exports_hash Hash of export definitions to be passed
+#   to psick::nfs::export
+#
+# @example Configure NFS server and export a directory:
+#   psick::profiles::linux_classes:
+#     nfs_server: psick::nfs::server
+#   psick::nfs::server::exports_hash:
+#     data:
+#       share: /data
+#       guest: 10.42.42.0/16
+#
+class psick::nfs::server (
+  Hash $exports_hash = {}
+) {
 
   tp::install { 'nfs-server': }
 
+  $exports_hash.each |$k,$v| {
+    psick::nfs::export { $k:
+      * => $v,
+    }
+  }
 }
