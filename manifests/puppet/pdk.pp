d8243137bd221e8dd8772dbcb916421daf1d262c
Alessandro Franceschi <al@example42.com>
2017-11-26 11:09:04 +0100

    Jenkins and openvpn developments
    * Manage jenkins ssh host keys, added psick::openssh::config
    
    * Sorting up timezone
    
    * Timezone single default entry point
    
    * Improvements on psick::jenkins::scm_sync
    
    * Minors
    
    * On jenkins, pdk and openvpn

--- manifests/puppet/pdk.pp
+++ manifests/puppet/pdk.pp
@@ -1,9 +1,42 @@
 #
 class psick::puppet::pdk (
-  $ensure = 'present',
+  Psick::Ensure $ensure = 'present',
+  #  String $dist,
+  #  String $extract_command,
+  Boolean         $auto_prereq              = $::psick::auto_prereq,
 ) {
+
+  if $auto_prereq {
+    include ::psick::ruby::buildgems
+  }
+
   # Waiting for pdk repo
   #  package { 'pdk':
   #   ensure => $ensure,
   #}
+  $dist = $facts['os']['family'] ? {
+    'RedHat' => 'el',
+    'SuSE'   => 'sles',
+    'Debian' => 'ubuntu',
+  }
+  $arch = $facts['os']['family'] ? {
+    'Debian' => 'amd64',
+    default  => 'x86_64',
+  }
+  $url = "https://pm.puppetlabs.com/cgi-bin/pdk_download.cgi?dist=${dist}&rel=${facts['os']['release']['major']}&arch=${arch}&ver=latest"
+  $command = $facts['os']['family'] ? {
+    'RedHat' => "rpm -Uvh '${url}'",
+    'SuSE'   => "rpm -Uvh '${url}'",
+    'Debian' => "wget '${url}' -O pdk.deb && dpkg --install pdk.deb",
+  }
+  $unless_command = $facts['os']['family'] ? {
+    'RedHat' => 'rpm -qi pdk',
+    'SuSE'   => 'rpm -qi pdk',
+    'Debian' => 'apt show pdk',
+  }
+
+  exec { 'Install PDK':
+    command => $command,
+    unless  => $unless_command,
+  }
 }
