c5ab584c6e8f0b3bc7ee44f1de77ec6ab79bf118
Alessandro Franceschi <al@example42.com>
2017-12-04 08:01:31 +0100

    Added configration of Jenkins init params and disable config wizard

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -7,6 +7,8 @@ class psick::jenkins (
 
   Hash                       $plugins    = {},
 
+  Hash                       $init_options  = {},
+
   Optional[String] $ssh_private_key_content = undef,
   Optional[String] $ssh_public_key_content  = undef,
   Optional[String] $ssh_private_key_source  = undef,
@@ -17,7 +19,32 @@ class psick::jenkins (
 
   Optional[String] $scm_sync_repository_url  = undef,
   Optional[String] $scm_sync_repository_host = undef,
+
+  Boolean $disable_setup_wizard              = false,
 ) {
+
+  $java_args_extra = $disable_setup_wizard ? {
+    true  => '-Djenkins.install.runSetupWizard=false',
+    false => '',
+  }
+
+  $default_init_options = {
+    'NAME'           => 'jenkins',
+    'JAVA'           => '/usr/bin/java',
+    'JAVA_ARGS'      => "-Djava.awt.headless=true ${java_args_extra}",
+    'PIDFILE'        => '/var/run/$NAME/$NAME.pid',
+    'JENKINS_USER'   => '$NAME',
+    'JENKINS_GROUP'  => '$NAME',
+    'JENKINS_WAR'    => '/usr/share/$NAME/$NAME.war',
+    'JENKINS_HOME'   => '/var/lib/$NAME',
+    'RUN_STANDALONE' => 'true',
+    'JENKINS_LOG'    => '/var/log/$NAME/$NAME.log',
+    'MAXOPENFILES'   => '8192',
+    'HTTP_PORT'      => '8080',
+    'PREFIX'         => '/$NAME',
+    'JENKINS_ARGS'   => '--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT',
+  }
+  $all_init_options = $default_init_options + $init_options
 
   # Installation management
   case $module {
@@ -30,6 +57,11 @@ class psick::jenkins (
           *       => $v,
         }
       }
+      tp::conf { 'jenkins::init':
+        template     => 'psick/jenkins/init.erb',
+        options_hash => $all_init_options,
+        base_file    => 'init',
+      }
     }
     default: {
       contain ::jenkins
@@ -41,7 +73,8 @@ class psick::jenkins (
   or $ssh_private_key_content
   or $ssh_public_key_content
   or $ssh_private_key_source
-  or $ssh_public_key_source {
+  or $ssh_public_key_source
+  or $scm_sync_repository_host {
     $dir_ensure = ::tp::ensure2dir($ensure)
     file { "${home_dir}/.ssh" :
       ensure  => $dir_ensure,
@@ -101,4 +134,5 @@ class psick::jenkins (
       }
     }
   }
+
 }
