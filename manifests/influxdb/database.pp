9dfe4f71594ba8bf6b9296f2887c8d9ca37a58ea
Alessandro Franceschi <al@example42.com>
2018-05-31 23:19:03 +0200

    Influxdb defines refactoring

--- manifests/influxdb/database.pp
+++ manifests/influxdb/database.pp
@@ -1,23 +1,89 @@
-
-
+# Define psick::influxdb::database
+# 
+# @summary This define creates a influxdb database
+#
+# @example Create a database on local server
+#   psick::influxdb::database { 'my_data': }
+#
+# @example Create a user on remote server
+#   psick::influxdb::database { 'my_data': }
+#     server_host     => 'influxdb',
+#     server_user     => 'admin',
+#     server_password => 'pw'
+#   }
+#
+#  @param database The database to manage. Default is = $title
+#  @param ensure If to create or remove the database
+#  @param server_host The host of the influxdb server to connect to.
+#    If not specified influx cli defaults are used.
+#  @param server_port The port of the influxdb server to connect to.
+#    If not specified influx cli defaults are used.
+#  @param server_user The user to use to connect to server.
+#    If not specified influx cli defaults are used.
+#  @param server_password The password of the connection user.
+#    If not specified influx cli defaults are used.
+#  @param exec_params An hash of params to set or override the
+#    arguments passed to the exec resources which runs
+#    influx command.
+#
 define psick::influxdb::database (
   Enum['present', 'absent'] $ensure = 'present',
+  String                  $database = $title,
+  Optional[String] $server_host     = 'localhost',
+  Optional[String] $server_port     = undef,
+  Optional[String] $server_user     = undef,
+  Optional[String] $server_password = undef,
+  Hash $exec_params                 = {},
 ){
-  ensure_packages(['influxdb-client'])
+
+  include psick::influxdb::client
+
+  # Build command line arguments
+  $host_param = "-host '${server_host}'"
+  $port_param = $server_port ? {
+    undef   => '',
+    default => "-port '${server_port}'",
+  }
+  $password_param = $server_password ? {
+    undef   => '',
+    default => "-password '${server_password}'",
+  }
+  $user_param = $server_user ? {
+    undef   => '',
+    default => "-user '${server_user}'",
+  }
+
+  $influx_params = "${host_param} ${port_param} ${password_param} ${user_param}"
+
   if $ensure == 'present' {
-    $cmd_1 = "CREATE DATABASE ${name} "
-
-    exec {"create_db_${name}":
-      command => "/usr/bin/influx -execute '${cmd_1}'",
-      unless  =>  "/usr/bin/influx -execute \"SHOW DATABASES\" | grep \"^${name}$\"",
-    }
+    $exec_title = "Create database ${title}"
+    $_cmd = "CREATE DATABASE ${database}"
+    $exec_command = "/usr/bin/influx ${influx_params} -execute \"${_cmd}\""
+    $exec_unless  = "/usr/bin/influx -execute \"SHOW DATABASES\" | grep --perl-regex \"^${name}\t\""
+    $exec_onlyif  = undef
   } else {
-    $cmd_1 = "DROP DATABASE ${name} "
+    $exec_title = "Drop database ${title}"
+    $_cmd = "DROP DATABASE ${database} "
+    $exec_command = "/usr/bin/influx ${influx_params} -execute \"${_cmd}\""
+    $exec_unless  = undef
+    $exec_onlyif  = "/usr/bin/influx -execute \"SHOW DATABASES\" | grep --perl-regex \"^${name}\t\""
+  }
 
-    exec {"drop_db_${name}":
-      command => "/usr/bin/influx -execute '${cmd_1}'",
-      onlyif  =>  "/usr/bin/influx -execute \"SHOW DATABASES\" | grep \"^${name}$\"",
-    }
+  # Attempt to autoconfigure dependencies based on server host. Can be
+  # overridden with param $exec_params
+  $exec_require = $server_host ? {
+    /(localhost|127.0.0.1|$fqdn|$hostname|$ipaddress)/ => [Package[influxdb-client],Package[influxdb],Service[influxdb]],
+    default                                            => 'Package[influxdb-client]',
+  }
+  $exec_default_options = {
+    'command' => $exec_command,
+    'unless'  => $exec_unless,
+    'onlyif'  => $exec_onlyif,
+    'require' => $exec_require,
+  }
+
+  exec { $exec_title:
+    * => $exec_default_options + $exec_params,
   }
 
 }
