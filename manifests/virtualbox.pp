679af0d16ce0602ef7bc8c1b9755ee90184bea2f
root <root@lob.psick.io>
2018-11-10 06:57:06 +0100

    psick::virtualbox updated

--- manifests/virtualbox.pp
+++ manifests/virtualbox.pp
@@ -1,44 +1,34 @@
-# This class manages the installation and configuration of virtualbox via tp
-#
-# @param ensure If to install or remove the virtualbox package
-# @param config_dir_source The source to use (as used in source =>) to populate
-#                          the whole virtualbox configuration directory
-# @param config_file_template The erb template (as used in template()) to manage
-#                             the content of the virtualbox main.cf file
-# @param options An open hash of options to use in the provided template.
-#                Note: This variable is not a class paramenter but it's looked
-#                up with hiera_hash('psick::logs::virtualbox::options', {} )
+# @class virtualbox
 #
 class psick::virtualbox (
-  Enum['present','absent'] $ensure                     = 'present',
 
-  Variant[String[1],Undef] $config_dir_source          = undef,
-  String                   $config_file_template       = '',
-  Boolean                  $auto_prereq                = $::psick::auto_prereq,
+  Variant[Boolean,String]    $ensure = present,
+  Enum['psick']              $module = 'psick',
+
+  Optional[Psick::Password]  $root_password = undef,
+
+  Hash                       $network_hash  = {},
+  Hash                       $vm_hash    = {},
 ) {
 
-  $options_default = {
-  }
-  $options_user=hiera_hash('psick::mail::virtualbox::options', {} )
-  $options=merge($options_default,$options_user)
-
-  # Postfix as local mailer
-  ::tp::install { 'virtualbox':
-    ensure      => $ensure,
-    auto_repo   => $auto_prereq,
-    auto_prereq => $auto_prereq,
-  }
-
-  if $config_file_template != '' {
-    ::tp::conf { 'virtualbox':
-      ensure       => $ensure,
-      template     => $config_file_template,
-      options_hash => $options,
+  # Intallation management
+  case $module {
+    'psick': {
+      contain ::psick::virtualbox::tp
+      $vm_hash.each |$k,$v| {
+        psick::virtualbox::vm { $k:
+          * => $v,
+        }
+      }
+      $network_hash.each |$k,$v| {
+        psick::virtualbox::network { $k:
+          * => $v,
+        }
+      }
+    }
+    default: {
+      contain ::virtualbox
     }
   }
 
-  ::tp::dir { 'virtualbox':
-    ensure => $ensure,
-    source => $config_dir_source,
-  }
 }
