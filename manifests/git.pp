9537047b1da5a926ae6034e4714f1174aa026f3c
Alessandro Franceschi <al@example42.com>
2018-12-13 14:04:51 +0100

    Added extra_packages_list to psick::git

--- manifests/git.pp
+++ manifests/git.pp
@@ -3,8 +3,9 @@
 # @param ensure Define if to install or remove git
 #
 class psick::git (
-  Enum['present','absent'] $ensure       = 'present',
-  Hash                     $configs_hash = {},
+  Enum['present','absent'] $ensure              = 'present',
+  Hash                     $configs_hash        = {},
+  Array                    $extra_packages_list = [],
 ) {
   tp::install { 'git':
     ensure => $ensure,
@@ -12,6 +13,11 @@ class psick::git (
   $configs_hash.each |$k,$v| {
     psick::git::config { $k:
       * => $v,
+    }
+  }
+  $extra_packages_list.each |$k| {
+    package { $k:
+      ensure => $ensure,
     }
   }
 }
