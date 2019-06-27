e94a63430226f7f9debc72738edd4647dd9ca901
Alessandro Franceschi <al@example42.com>
2019-06-27 17:42:29 +0200

    Bolt and network

--- manifests/bolt/project.pp
+++ manifests/bolt/project.pp
@@ -1,9 +1,37 @@
-# @summary A short summary of the purpose of this defined type.
+# @summary This define creates a bolt project directory.
 #
-# A description of what this defined type does
+# It allows to customise the contents of all the relevant files
+# and it can be integrated over a Puppet control-repo, to clone
+# from a given $control_repo_url.
+# By default everything is cretaed in a directory called Boltdir in
+# the home passed as title. It's possible to specify a different $user
+# (using whatever title) or a different destination $path 
 #
-# @example
+# @example Create a bolt project in /home/al/Boltdir (on *nix)
+# 
 #   psick::bolt::project { 'al': }
+#
+# @example Create a bolt project in /opt/admins/bolt-control-center as user admin
+#   
+#   psick::bolt::project { 'control-center':
+#     user => 'admin',
+#     path => '/opt/admins/bolt-control-center',
+#   }
+#
+# @example Create a bolt project in /home/al/psick based on psick control-repo
+#
+#   psick::bolt::project { 'al':
+#     path                   => '/home/al/psick',
+#     control_repo_integrate => true,
+#   }
+#
+# @example Create a bolt project in /home/al/Boltdir based on pa custom control-repo
+#
+#   psick::bolt::project { 'al':
+#     control_repo_integrate => true,
+#     control_repo_url       => 'https://git.example/puppet/control-repo',
+#   }
+#
 define psick::bolt::project (
 
   Psick::Ensure $ensure                = 'present',
@@ -11,6 +39,9 @@ define psick::bolt::project (
   Optional[String] $group              = undef,
   String $mode                         = '0640',
   Boolean $replace                     = true,
+
+  Boolean $user_manage                 = false,
+  Hash $user_options_hash              = {}, # Hash of options for psick::users::managed
 
   Optional[Stdlib::Absolutepath] $path = undef,
 
@@ -24,11 +55,13 @@ define psick::bolt::project (
   Optional[String] $data_repo_url = undef,
 
   Boolean $control_repo_integrate = false,
-  Stdlib::Url $control_repo_url = 'https://github.com/example42/psick',
+  Stdlib::Url $control_repo_url   = 'https://github.com/example42/psick',
+
+  Boolean $puppetfile_install = false,
 
 ) {
 
-  $bolt_dir = pick($path, psick::get_user_home($user))
+  $bolt_dir = pick($path, psick::get_user_home($user)) + '/Boltdir'
 
   File {
     ensure  => $ensure,
@@ -42,29 +75,52 @@ define psick::bolt::project (
     }
   }
 
+  if $control_repo_integrate {
+    tp::dir { $bolt_dir:
+      ensure  => $ensure,
+      source  => $control_repo_url,
+      vcsrepo => 'git',
+    }
+  } else {
+    file { $bolt_dir:
+      ensure => tp::ensure2dir($ensure)
+    }
+  }
+
   $default_config_hash = {
-    [ "${bolt_dir}/modules" , "${bolt_dir}/site-modules" ] => {
+    [ 'modules' , 'site-modules' ] => {
       ensure => tp::ensure2dir($ensure)
     },
-    "${bolt_dir}/bolt.yaml" => {
+    'bolt.yaml' => {
       content => psick::template($bolt_yaml_template),
     },
-    "${bolt_dir}/inventory.yaml" => {
+    'inventory.yaml' => {
       content => psick::template($inventory_yaml_template),
     },
   }
 
   if $data_repo_url {
+    # tp::dir handles file resource data
+    tp::dir { "${bolt_dir}/data":
+      source  => $data_repo_url,
+      vcsrepo => 'git',
+    }
     $data_dir_config_hash = { }
   } else {
     if $use_default_config_files_hash {
       $data_dir_config_hash = {
-        "${bolt_dir}/data" => {
+        'data' => {
           ensure => tp::ensure2dir($ensure)
         }
       }
     } else {
       $data_dir_config_hash = { }
+    }
+  }
+
+  if $user_manage {
+    user { $user:
+      * => $user_options_hash,
     }
   }
 
@@ -79,22 +135,11 @@ define psick::bolt::project (
     }
   }
 
-  if $control_repo_integrate {
-    tp::dir { $bolt_dir:
-      source  => $control_repo_url,
-      vcsrepo => 'git',
-    }
-  } else {
-    file { $bolt_dir:
-      ensure => tp::ensure2dir($ensure)
+  if $puppetfile_install {
+    exec { 'bolt install puppetfile':
+      path    => $::path,
+      cwd     => $bolt_dir,
+      creates => 'modules/stdlib', # Quick and very dirty
     }
   }
-
-  if $data_repo_url {
-    tp::dir { "${bolt_dir}/data":
-      source  => $data_repo_url,
-      vcsrepo => 'git',
-    }
-  }
-
 }
--- manifests/network.pp
+++ manifests/network.pp
@@ -16,58 +16,72 @@
 class psick::network (
   String $bonding_mode     = 'active-backup',
   String $network_template = 'psick/network/network.erb',
+  Hash $routes             = {},
+  Hash $interfaces         = {},
+  Boolean $use_netplan     = false,
 ) {
 
   contain ::network
 
-  file { '/etc/modprobe.d/bonding.conf':
-    ensure => file,
-  }
-  $routes = hiera_hash('psick::network::routes', {})
-  $routes.each |$r,$o| {
-    ::network::mroute { $r:
-      routes => $o[routes],
-    }
-  }
-  $default_options = {
-    onboot     => 'yes',
-    'type'     => 'Ethernet',
-    template   => "psick/network/interface-${::osfamily}.erb",
-    options    => {
-      'IPV6INIT'           => 'no',
-      'IPV4_FAILURE_FATAL' => 'yes',
-    },
-    bootproto  => 'none',
-    nozeroconf => 'yes',
-  }
-  $default_bonding_options = {
-    'type'         => 'Bond',
-    bonding_opts   => "resend_igmp=1 updelay=30000 use_carrier=1 miimon=100 downdelay=100 xmit_hash_policy=0 primary_reselect=0 fail_over_mac=0 arp_validate=0 mode=${bonding_mode} arp_interval=0 ad_select=0",
-    bonding_master => 'yes',
-  }
-  $interfaces = hiera_hash('psick::network::interfaces', {})
-  $interfaces.each |$r,$o| {
-    if $r =~ /^bond/ {
-      $options = $default_options + $default_bonding_options + $o
-      file_line { "bonding.conf ${r}":
-        line    => "alias netdev-${r} bonding",
-        path    => '/etc/modprobe.d/bonding.conf',
-        require => File['/etc/modprobe.d/bonding.conf'],
+  if $use_netplan {
+    $interfaces.each |$r,$o| {
+      if $r =~ /^bond/ {
+        $options = $default_options + $default_bonding_options + $o
+      } else {
+        $options = $default_options + $o
       }
-    } else {
-      $options = $default_options + $o
+      ::network::netplan::interface { $r:
+        * => $options,
+      }
     }
-    ::network::interface { $r:
-      * => $options,
+  } else {
+    file { '/etc/modprobe.d/bonding.conf':
+      ensure => file,
     }
-  }
+    $routes.each |$r,$o| {
+      ::network::mroute { $r:
+        routes => $o[routes],
+      }
+    }
+    $default_options = {
+      onboot     => 'yes',
+      'type'     => 'Ethernet',
+      template   => "psick/network/interface-${::osfamily}.erb",
+      options    => {
+        'IPV6INIT'           => 'no',
+        'IPV4_FAILURE_FATAL' => 'yes',
+      },
+      bootproto  => 'none',
+      nozeroconf => 'yes',
+    }
+    $default_bonding_options = {
+      'type'         => 'Bond',
+      bonding_opts   => "resend_igmp=1 updelay=30000 use_carrier=1 miimon=100 downdelay=100 xmit_hash_policy=0 primary_reselect=0 fail_over_mac=0 arp_validate=0 mode=${bonding_mode} arp_interval=0 ad_select=0",  # lint:ignore:140chars
+      bonding_master => 'yes',
+    }
+    $interfaces.each |$r,$o| {
+      if $r =~ /^bond/ {
+        $options = $default_options + $default_bonding_options + $o
+        file_line { "bonding.conf ${r}":
+          line    => "alias netdev-${r} bonding",
+          path    => '/etc/modprobe.d/bonding.conf',
+          require => File['/etc/modprobe.d/bonding.conf'],
+        }
+      } else {
+        $options = $default_options + $o
+      }
+      ::network::interface { $r:
+        * => $options,
+      }
+    }
 
-  if $::osfamily == 'RedHat'
-  and $network_template != ''
-  and $::profile::base::linux::hostname_class != '' {
-    file { '/etc/sysconfig/network':
-      ensure  => file,
-      content => template($network_template),
+    if $::osfamily == 'RedHat'
+    and $network_template != ''
+    and $::profile::base::linux::hostname_class != '' {
+      file { '/etc/sysconfig/network':
+        ensure  => file,
+        content => template($network_template),
+      }
     }
   }
 }
