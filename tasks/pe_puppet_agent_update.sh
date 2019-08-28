dca7e7be8262c48b61b7ef45207bbd64ff214d9b
Alessandro Franceschi <al@example42.com>
2019-08-28 17:50:41 +0200

    Make pe_puppet_agent_update aware of HA setups

--- tasks/pe_puppet_agent_update.sh
+++ tasks/pe_puppet_agent_update.sh
@@ -2,7 +2,12 @@
 if [[ -n "${PT_puppet_server}" ]]; then
   puppet_server=$PT_puppet_server
 else
+  puppet_server=$(grep "^server_list"  /etc/puppetlabs/puppet/puppet.conf | cut -d '=' -f 2 | cut -d ':' -f 1 | sed -e 's/^[ \t]*//')
+fi
+
+if [[ "x${puppet_server}" == "x" ]]; then
   puppet_server=$(/opt/puppetlabs/puppet/bin/puppet config print server)
 fi
-/opt/puppetlabs/puppet/bin/curl -k --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem "https://${puppet_server}:8140/packages/current/install.bash" | bash
+echo "## Fetching install script from ${puppet_server}"
 
+/opt/puppetlabs/puppet/bin/curl --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem "https://${puppet_server}:8140/packages/current/install.bash" | bash
