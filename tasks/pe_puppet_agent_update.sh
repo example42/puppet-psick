769c2953b18aa48b4e20b79a315ab9869705ab8b
Alessandro Franceschi <al@example42.com>
2019-08-28 13:20:37 +0200

    Updated pe_puppet_agent_update task with puppet_server argument

--- tasks/pe_puppet_agent_update.sh
+++ tasks/pe_puppet_agent_update.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [[ -n "${PT_puppet_server}" ]]; then
+  puppet_server=$PT_puppet_server
+else
+  puppet_server=$(/opt/puppetlabs/puppet/bin/puppet config print server)
+fi
+/opt/puppetlabs/puppet/bin/curl -k --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem "https://${puppet_server}:8140/packages/current/install.bash" | bash
 
-server=$(puppet config print server)
-/opt/puppetlabs/puppet/bin/curl --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem https://$server:8140/packages/current/install.bash | bash
