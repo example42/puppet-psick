abb4fbd75283442282dd9191bc5f1409edbd292f
Alessandro Franceschi <al@example42.com>
2019-10-18 10:36:45 +0200

    Better psick::ostest, with tp_profile entrypoint

--- manifests/ostest.pp
+++ manifests/ostest.pp
@@ -2,12 +2,25 @@
 # Used to code testing on different OS
 #
 class psick::ostest (
-
+  Boolean $manage        = $::psick::manage,
   Boolean $notify_enable = false,
-
+  Hash $tp_profiles_hash = {},
+  Boolean $no_noop       = false,
 ) {
+  if $manage {
+    if !$::psick::noop_mode and $no_noop {
+      info('Forced no-noop mode in psick::icinga2')
+      noop(false)
+    }
 
-  if $notify_enable {
-    notify { 'ostest role': }
+    if $notify_enable {
+      notify { 'ostest role': }
+    }
+
+    $tp_profiles_hash.each |$k,$v| {
+      class { "tp_profile::${k}":
+        * => $v,
+      }
+    }
   }
 }
