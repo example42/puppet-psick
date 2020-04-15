b12290dc66c1baf702c6c10d42ec75753a30200e
Alessandro Franceschi <al@example42.com>
2020-04-15 17:38:06 +0200

    Fixed tests

--- spec/classes/kmod_spec.rb
+++ spec/classes/kmod_spec.rb
@@ -1,7 +1,4 @@
 require 'spec_helper'
-require 'yaml'
-facts_yaml = File.dirname(__FILE__) + '/../../fixtures/facts/spec.yaml'
-facts = YAML.load_file(facts_yaml)
 
 describe 'psick::kmod' do
   on_supported_os(facterversion: '2.4').select { |k, _v| k == 'redhat-7-x86_64' || k == 'ubuntu-16.04-x86_64' }.each do |os, os_facts|
