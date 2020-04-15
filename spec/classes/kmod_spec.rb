b0e9b417948c63405336ca64a4b1457445105b1d
Alessandro Franceschi <al@example42.com>
2020-04-15 18:06:47 +0200

    Fix again

--- spec/classes/kmod_spec.rb
+++ spec/classes/kmod_spec.rb
@@ -1,22 +1,14 @@
 require 'spec_helper'
 
 describe 'psick::kmod' do
-  on_supported_os.select { |k, _v| k == 'redhat-7-x86_64' || k == 'ubuntu-16.04-x86_64' }.each do |os, os_facts|
+  on_supported_os.select { |k, _v| k == 'redhat-7-x86_64' || k == 'ubuntu-18.04-x86_64' }.each do |os, os_facts|
     context "on #{os}" do
-      let(:facts) { os_facts.merge(facts) }
+      let(:facts) { os_facts }
       let(:pre_condition) { 'include psick' }
 
       describe 'with default params' do
         it { is_expected.to compile }
-        it { is_expected.to contain_class('psick::kmod') }
       end
-
-      describe 'with manage => false' do
-        let(:params) { { 'manage' => false } }
-
-        it { is_expected.to have_resource_count(0) }
-      end
-
     end
   end
 end
