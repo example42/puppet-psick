711cd0ff3ec257b313e5593776b52e6301226cf0
Alessandro Franceschi <al@example42.com>
2020-11-26 22:09:00 +0100

    Added options to puppet_apply task to allow download of Puppet code from various sources

--- tasks/puppet_apply.sh
+++ tasks/puppet_apply.sh
@@ -1,9 +1,65 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
   declare -r pre_command='sudo '
 else
   declare -r pre_command=''
+fi
+
+if [[ "x${PT_git_source}" != "x" ]]; then
+  if [ -d ${PT_destination} ]; then
+    command -v git &>/dev/null || exit 1
+    cd $PT_destination
+    git pull
+  else
+    git clone $PT_git_source $PT_destination
+  fi
+fi
+
+if [[ "x${PT_zip_source}" != "x" ]]; then
+  if [ ! -d $PT_destination ]; then
+    command -v wget &>/dev/null || { echo "command wget not found!" && exit 1; }
+    command -v unzip &>/dev/null || { echo "command unzip not found!" && exit 1; }
+    mkdir -p $PT_destination
+    cd $PT_destination
+    TMPFILE=`mktemp`
+    wget "${PT_zip_source}" -O $TMPFILE
+    unzip -d $PT_destination $TMPFILE
+    rm -f $TMPFILE
+  else
+    echo "${PT_destination} exists. Not changing it. Remove it to unzip ${PT_zip_source} again"
+  fi
+fi
+
+if [[ "x${PT_tgz_source}" != "x" ]]; then
+  if [ ! -d $PT_destination ]; then
+    command -v wget &>/dev/null || { echo "command wget not found!" && exit 1; }
+    command -v tar &>/dev/null || { echo "command tar not found!" && exit 1; }
+    mkdir -p $PT_destination
+    cd $PT_destination
+    TMPFILE=`mktemp`
+    wget "${PT_tgz_source}" -O $TMPFILE
+    tar -zvxf $TMPFILE .
+    rm -f $TMPFILE
+  else
+    echo "${PT_destination} exists. Not changing it. Remove it to unzip ${PT_tgz_source} again"
+  fi
+fi
+
+if [[ "x${PT_puppet_source}" != "x" ]]; then
+  command -v puppet &>/dev/null || { echo "command puppet not found!" && exit 1; }
+  puppet apply -e "file { \"${PT_destination}\": source => \"${PT_puppet_source}\", ensure => directory, recurse => true }"
+fi
+
+if [[ "x${PT_puppet_source_tgz}" != "x" ]]; then
+  command -v puppet &>/dev/null || { echo "command puppet not found!" && exit 1; }
+  command -v tar &>/dev/null || { echo "command tar not found!" && exit 1; }
+  TMPFILE=`mktemp`
+  puppet apply -e "file { \"${TMPFILE}\": source => \"${PT_puppet_source_tgz}\", ensure => present }"
+  mkdir -p $PT_destination
+  cd $PT_destination
+  tar -zvxf $TMPFILE .
+  rm -f $TMPFILE
 fi
 
 declare puppet_options
