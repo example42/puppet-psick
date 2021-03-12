fd165b3b8c57163fe01bcebbb04d94f4eecaa1d9
Alessandro Franceschi <al@example42.com>
2021-03-12 20:57:44 +0100

    Fix sar for Suse

--- manifests/monitor/sar.pp
+++ manifests/monitor/sar.pp
@@ -5,6 +5,7 @@ class psick::monitor::sar (
   String $check_cron    = '*/5 * * * *',
   String $summary_cron  = '53 23 * * *',
   String $cron_template = 'psick/sar/systat.cron.erb',
+  Optional[String] $package_name = 'sysstat',
   Boolean $manage       = $::psick::manage,
   Boolean $noop_manage  = $::psick::noop_manage,
   Boolean $noop_value   = $::psick::noop_value,
@@ -13,12 +14,14 @@ class psick::monitor::sar (
     if $noop_manage {
       noop($noop_value)
     }
-    package { 'sysstat':
-      ensure => $ensure,
-    }
-    file { '/etc/cron.d/sysstat':
-      ensure  => $ensure,
-      content => template($cron_template),
+    if $package_name and $package_name != '' {
+      package { $package_name:
+        ensure => $ensure,
+      }
+      file { '/etc/cron.d/sysstat':
+        ensure  => $ensure,
+        content => template($cron_template),
+      }
     }
   }
 }
