638ebb2b33b2b8d236e9739e9e91a2962ad89af6
Alessandro Franceschi <al@example42.com>
2022-05-13 18:05:26 +0200

    Works on psick::network (#78)

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,5 +1,7 @@
-# This class manages network configurations
+# psick::network
 #
+# @summary This psick profile manages network settings, such as interfaces and
+# routes.
 # @param bonding_mode Define bonding mode (default: active-backup)
 # @param network_template The erb template to use, only on RedHad derivatives,
 #                         for the file /etc/sysconfig/network
@@ -13,88 +15,69 @@
 #                   options according to the interface type. You can override
 #                   them in the provided hash
 #
+# @example Include it to manage network resources
+#   include psick::network
+#
+# @example Include in PSICK via hiera (yaml)
+#   psick::profiles::linux_classes:
+#     network: psick::network
+#
+# @example Set no-noop mode and enforce changes even if noop is set for the agent
+#     psick::network::no_noop: true
+#
+# @param manage If to actually manage any resource in this profile or not
+# @param auto_prereq If to automatically install eventual dependencies.
+#   Set to false if you have problems with duplicated resources, being sure that you
+#   provide the needed prerequistes.
+# @param options_hash An custom hash of keypair which may be used in templates to
+#   manage any network setting.
+# @param module What module to use to manage network. By default psick is used.
+#   The specified module name, if different, must be added to Puppetfile. 
+# @param no_noop Set noop metaparameter to false to all the resources of this class.
+#   This overrides any noop setting which might be in place.
 class psick::network (
+  Psick::Ensure   $ensure               = 'present',
+  Boolean         $manage               = $psick::manage,
+  Boolean         $auto_prereq          = $psick::auto_prereq,
+  Hash            $options_hash         = {},
+  String          $module               = 'psick',
+  Boolean          $noop_manage         = $psick::noop_manage,
+  Boolean          $noop_value          = $psick::noop_value,
+
+  # Legacy params now passed tp psick::network::example42
   String $bonding_mode     = 'active-backup',
   String $network_template = 'psick/network/network.erb',
-  Hash $routes             = {},
-  Hash $interfaces         = {},
-  Boolean $use_netplan     = false,
 
-  Boolean          $manage               = $psick::manage,
-  Boolean          $noop_manage          = $psick::noop_manage,
-  Boolean          $noop_value           = $psick::noop_value,
+  # Generic Hashes of resources and options
+  Hash $interfaces_hash      = {},
+  Hash $interfaces_default_options_hash = {},
+
+  Hash $routes_hash      = {},
 
 ) {
+
+  # We declare resources only if $manage = true
   if $manage {
     if $noop_manage {
       noop($noop_value)
     }
-    contain network
 
-    if $use_netplan {
-      $default_options = {
-      }
-      $default_bonding_options = {
-      }
-      $interfaces.each |$r,$o| {
-        if $r =~ /^bond/ {
-          $options = $default_options + $default_bonding_options + $o
-        } else {
-          $options = $default_options + $o
-        }
-        network::netplan::interface { $r:
-          * => $options,
-        }
-      }
-    } else {
-      file { '/etc/modprobe.d/bonding.conf':
-        ensure => file,
-      }
-      $routes.each |$r,$o| {
-        ::network::mroute { $r: # With example42-network 3.x
-          # ::network::route { $r: # With example42-network 4.x
-          routes => $o[routes],
-        }
-      }
-      $default_options = {
-        onboot     => 'yes',
-        'type'     => 'Ethernet',
-        template   => "psick/network/interface-${facts['os']['family']}.erb",
-        options    => {
-          'IPV6INIT'           => 'no',
-          'IPV4_FAILURE_FATAL' => 'yes',
-        },
-        bootproto  => 'none',
-        nozeroconf => 'yes',
-      }
-      $default_bonding_options = {
-        'type'         => 'Bond',
-        bonding_opts   => "resend_igmp=1 updelay=30000 use_carrier=1 miimon=100 downdelay=100 xmit_hash_policy=0 primary_reselect=0 fail_over_mac=0 arp_validate=0 mode=${bonding_mode} arp_interval=0 ad_select=0",  # lint:ignore:140chars
-        bonding_master => 'yes',
-      }
-      $interfaces.each |$r,$o| {
-        if $r =~ /^bond/ {
-          $options = $default_options + $default_bonding_options + $o
-          file_line { "bonding.conf ${r}":
-            line    => "alias netdev-${r} bonding",
-            path    => '/etc/modprobe.d/bonding.conf',
-            require => File['/etc/modprobe.d/bonding.conf'],
+    # Managed resources according to $module selected
+    case $module {
+      'psick': {
+        $routes_hash.each |$r,$o| {
+          ::psick::network::route { $r:
+            * => $o,
           }
-        } else {
-          $options = $default_options + $o
         }
-        ::network::interface { $r:
-          * => $options,
+        $interfaces_hash.each |$i,$o| {
+          ::psick::network::interface { $i:
+            * => $interfaces_default_options_hash + $o,
+          }
         }
       }
-
-      if $facts['os']['family'] == 'RedHat'
-      and $network_template != ''
-      and $::profile::base::linux::hostname_class != '' {
-        file { '/etc/sysconfig/network':
-          ensure  => file,
-          content => template($network_template),
-        }
+      default: {
+        contain $module
       }
     }
   }
